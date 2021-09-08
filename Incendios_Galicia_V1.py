f24f235ececc6aac7f3cbcdff96702bcd33103a2
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-09-08 22:48:31 +0200

    Update Incendios_Galicia_V1.py

--- Incendios_Galicia_V1.py
+++ Incendios_Galicia_V1.py
@@ -195,7 +195,7 @@ if boton_prediccion:
   
   if result == 1: st.write('CAUSA incendio: **INTENCIONADO**')
   if result == 2: st.write('CAUSA incendio: **CAUSA DESCONOCIDA**')
-  if result == 3: st.write('CAUSA incendio: **NEGLIGENCIA INTENCIONADO**')
+  if result == 3: st.write('CAUSA incendio: **NEGLIGENCIA**')
   if result == 4: st.write('CAUSA incendio: **FUEGO REPRODUCIDO**')
   if result == 5: st.write('CAUSA incendio: **RAYO**')
   
