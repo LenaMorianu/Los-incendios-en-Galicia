e2f9aa06417c70f90f8ff728b8c7c9defe210abe
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-09-08 22:47:18 +0200

    Update Incendios_Galicia_V1.py

--- Incendios_Galicia_V1.py
+++ Incendios_Galicia_V1.py
@@ -104,7 +104,7 @@ classifier.score(X_test, y_test)
 
 y_pred = classifier.predict(X_test)
 LABELS=['Intencionado', 'Causa desconocida', 'Negligencia', 'Fuego reproducido', ' Rayo']
-st.write(print(classification_report(y_test, y_pred))
+st.write(print(classification_report(y_test, y_pred)))
 
 
 
