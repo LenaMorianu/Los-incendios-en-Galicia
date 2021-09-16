e4c34e752e48a6c05c62bff6913fd620e9dd4530
LenaMorianu <65896439+LenaMorianu@users.noreply.github.com>
2021-09-16 20:15:02 +0200

    Update Incendios_Galicia.py

--- Incendios_Galicia.py
+++ Incendios_Galicia.py
@@ -6,6 +6,7 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import bokeh
 from bokeh.plotting import figure
 #import pickle
